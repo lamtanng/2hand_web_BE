d1aabbc07927cba6cc38b50e143fa5e54810eba7
Lương Bùi <117280383+LuongBui1311@users.noreply.github.com>
2024-10-02 21:07:12 +0700

    Delete dist directory

--- dist/constants/routes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SIGNUP_ROUTE = exports.LOGIN_ROUTE = exports.V1_ROUTE = void 0;
-exports.V1_ROUTE = '/v1';
-exports.LOGIN_ROUTE = '/login';
-exports.SIGNUP_ROUTE = '/signup';
