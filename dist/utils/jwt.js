29be4e17d2314446e96817fcd3cffe6df47f68cd
Lương Bùi <117280383+LuongBui1311@users.noreply.github.com>
2024-10-03 17:29:41 +0700

    Delete dist directory

--- dist/utils/jwt.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getSecretKeyFromEnv = void 0;
-const getSecretKeyFromEnv = (secretKey) => secretKey.replace(/\\n/g, '\n');
-exports.getSecretKeyFromEnv = getSecretKeyFromEnv;
