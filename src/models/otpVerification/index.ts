05a6661dcf937f65d5b00406947fa4e3ffe6d7cd
lamtanng <laam122003@gmail.com>
2024-10-09 17:39:35 +0700

    feat: created pre middleware to save expireAt for OTP

--- src/models/otpVerification/index.ts
+++ src/models/otpVerification/index.ts
@@ -4,6 +4,10 @@ import {
   OTP_VERIFICATION_COLLECTION_SCHEMA,
 } from './otpVerification.doc';
 
+OTP_VERIFICATION_COLLECTION_SCHEMA.pre('save', async function () {
+  this.expiredAt = new Date();
+});
+
 export const OTPVerificationModel = mongoose.model(
   OTP_VERIFICATION_COLLECTION_NAME,
   OTP_VERIFICATION_COLLECTION_SCHEMA,
