5fe9d99515fff58bfcb53a4f40a013ae45e4634b
lamtanng <laam122003@gmail.com>
2024-10-09 14:18:21 +0700

    feat/signup: create TTL index for OTP

--- src/models/otpVerification/index.ts
+++ src/models/otpVerification/index.ts
@@ -1,14 +1,8 @@
 import mongoose from 'mongoose';
-import ms from 'ms';
 import {
   OTP_VERIFICATION_COLLECTION_NAME,
   OTP_VERIFICATION_COLLECTION_SCHEMA,
 } from './otpVerification.doc';
-
-//middleware
-OTP_VERIFICATION_COLLECTION_SCHEMA.pre('save', async function () {
-  this.expiredAt = new Date(this.createdAt.getTime() + ms('120s'));
-});
 
 export const OTPVerificationModel = mongoose.model(
   OTP_VERIFICATION_COLLECTION_NAME,
--- src/models/otpVerification/otpVerification.doc.ts
+++ src/models/otpVerification/otpVerification.doc.ts
@@ -1,36 +1,33 @@
 import mongoose, { Schema } from 'mongoose';
 import { OTPVerificationProps } from '../../types/model/otpVerification.type';
 import { USER_COLLECTION_NAME } from '../user/user.doc';
-import ms from 'ms';
 
 export interface IOtpVerificationMethods {
   comparePassword: (password: string) => Promise<boolean>;
 }
 export const OTP_VERIFICATION_COLLECTION_NAME = 'otpVerification';
-export const OTP_VERIFICATION_COLLECTION_SCHEMA = new Schema<OTPVerificationProps>(
-  {
-    _id: {
-      type: mongoose.Schema.Types.ObjectId,
-      default: function () {
-        return new mongoose.Types.ObjectId();
-      },
-    },
-    otp: {
-      type: String,
-      required: true,
-    },
-    userID: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: USER_COLLECTION_NAME,
-    },
-    email: {
-      type: String,
-      required: true,
-    },
-    expiredAt: {
-      type: Date,
-      default: new Date(Date.now() + ms('120s')),
+export const OTP_VERIFICATION_COLLECTION_SCHEMA = new Schema<OTPVerificationProps>({
+  _id: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: function () {
+      return new mongoose.Types.ObjectId();
     },
   },
-  { timestamps: true },
-);
+  otp: {
+    type: String,
+    required: true,
+  },
+  userID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: USER_COLLECTION_NAME,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  expiredAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+OTP_VERIFICATION_COLLECTION_SCHEMA.index({ expiredAt: 1 }, { expireAfterSeconds: 120 });
