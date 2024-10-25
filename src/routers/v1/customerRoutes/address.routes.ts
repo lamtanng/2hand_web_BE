1ee717a4487d048948724ef6c31f03b1bc0310f1
lamtanng <laam122003@gmail.com>
2024-10-25 17:20:34 +0700

    feat/RBAC: updated checkRolePermission function to accepting multple role

--- src/routers/v1/customerRoutes/address.routes.ts
+++ src/routers/v1/customerRoutes/address.routes.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { ActionPermission } from '../../../constants/actionPermission';
 import { userController } from '../../../controllers/user.controller';
-import { checkRolePermission } from '../../../middlewares/auth.middleware';
+import { checkCustomerPermission } from '../../../middlewares/auth.middleware';
 import { userAddressValidation } from '../../../validations/user.validation';
 const router = express.Router();
 
@@ -10,10 +10,8 @@ const { Read, Create, Update, Delete } = ActionPermission.User;
 
 router
   .route('/')
-  .post(checkRolePermission(Update), userAddressValidation, createReceiveAddress);
-router
-  .route('/')
-  .put(checkRolePermission(Update), userAddressValidation, updateAddress);
-router.route('/:addressID').delete(checkRolePermission(Update), deleteAddress);
+  .post(checkCustomerPermission(Update), userAddressValidation, createReceiveAddress);
+router.route('/').put(checkCustomerPermission(Update), userAddressValidation, updateAddress);
+router.route('/:addressID').delete(checkCustomerPermission(Update), deleteAddress);
 
 export const userAddressRoutes = router;
