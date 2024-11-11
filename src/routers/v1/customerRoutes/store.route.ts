2bef1654558a1d218f98cfb9d0b5903baedee13b
lamtanng <laam122003@gmail.com>
2024-11-11 22:18:50 +0700

    perf: added validation for creating store

--- src/routers/v1/customerRoutes/store.route.ts
+++ src/routers/v1/customerRoutes/store.route.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import { storeController } from '../../../controllers/store.controller';
-import { storeValidation } from '../../../validations/store.validation';
 import { isAuthorized } from '../../../middlewares/auth.middleware';
 import { STORE_BY_ID_ROUTE, STORE_BY_USER_ROUTE } from '../../../constants/routes';
+import { storeValidation } from '../../../validations/store.validation';
 
 const router = express.Router();
+
+const { createStoreValidation } = storeValidation;
 const { addStore, findAll, findOneById, findOneByUserId } = storeController;
 router.route('/').get(findAll);
-router.route('/').post(addStore);
+router.route('/').post(createStoreValidation, addStore);
 router.route(STORE_BY_ID_ROUTE).get(findOneById);
 router.route(STORE_BY_USER_ROUTE).get(findOneByUserId);
 
