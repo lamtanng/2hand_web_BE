8228c0b9042589784add959b7f2a3c4d05dc519c
lamtanng <laam122003@gmail.com>
2024-11-17 09:45:05 +0700

    perf: added validator for update info api

--- src/routers/v1/customerRoutes/user.routes.ts
+++ src/routers/v1/customerRoutes/user.routes.ts
@@ -1,6 +1,12 @@
 import express from 'express';
 import { ActionPermission } from '../../../constants/actionPermission';
-import { SEND_OTP_ROUTE, USER_ADDRESS_ROUTE, USER_BY_ID_ROUTE, USER_ROUTE, VERIFY_OTP_ROUTE } from '../../../constants/routes';
+import {
+  SEND_OTP_ROUTE,
+  USER_ADDRESS_ROUTE,
+  USER_BY_ID_ROUTE,
+  USER_ROUTE,
+  VERIFY_OTP_ROUTE,
+} from '../../../constants/routes';
 import { userController } from '../../../controllers/user.controller';
 import {
   checkAdminPermission,
@@ -22,13 +28,14 @@ const {
 } = userController;
 const { Read, Create, Update, Delete } = ActionPermission.User;
 const { verifySmsOTP } = otpMiddleware;
-const {userModelValidation,sendSmsOtpValidation,verifySmsOtpValidation } = userValidation
+const { userModelValidation, sendSmsOtpValidation, verifySmsOtpValidation, updateUserValidation } =
+  userValidation;
 
 router.route('/').get(checkAdminPermission(Read), findAll); //for admin
 router.route(USER_BY_ID_ROUTE).get(findOneById);
 router.route('/').post(checkCustomerPermission(Create), userModelValidation, addUser);
-router.route('/').put(checkCustomerPermission(Update), userModelValidation, updateUserInfo);
-router.route(SEND_OTP_ROUTE).post(sendSmsOtpValidation,sendSmsOtp);
+router.route('/').put(checkCustomerPermission(Update), updateUserValidation, updateUserInfo);
+router.route(SEND_OTP_ROUTE).post(sendSmsOtpValidation, sendSmsOtp);
 router.route(VERIFY_OTP_ROUTE).post(verifySmsOtpValidation, verifySmsOTP, createUserPhone);
 router.use(USER_ADDRESS_ROUTE, userAddressRoutes);
 
