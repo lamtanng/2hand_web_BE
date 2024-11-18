0c7afbfb48faa2520c50e61067ff8b6cead7fb7d
lamntjam <lamnt.flamedia@gmail.com>
2024-11-18 21:20:43 +0700

    feat: uploaded the avatar for both updating and adding an user

--- src/types/http/user.type.ts
+++ src/types/http/user.type.ts
@@ -7,5 +7,4 @@ export interface GetUsersResponseProps extends PaginationResponseProps {
 }
 
 export interface UpdateUserInfoRequestProps
-  extends Pick<UserProps,'_id'| 'firstName' | 'lastName' | 'dateOfBirth'> {}
-
+  extends Pick<UserProps, '_id' | 'firstName' | 'lastName' | 'dateOfBirth' | 'avatar'> {}
--- src/validations/user.validation.ts
+++ src/validations/user.validation.ts
@@ -1,16 +1,16 @@
 import { NextFunction, Request, Response } from 'express';
 import Joi from 'joi';
 import { isValidPhoneNumber } from 'libphonenumber-js';
-import { ObjectIDRegex } from '../constants/validation';
+import { ObjectIDRegex, PasswordRegex } from '../constants/validation';
 import { SendSmsOtpRequestProps, VerifySmsOtpRequestProps } from '../types/http/otp.type';
 import { UserProps } from '../types/model/user.type';
 import { catchErrors } from '../utils/catchErrors';
 import { addressValidation } from './address.validation';
 import { CommonValidation } from './common.validation';
+import { UpdateUserInfoRequestProps } from '../types/http/user.type';
 
 interface UserSchema extends UserProps {}
-interface UpdateUserSchema
-  extends Pick<UserProps, '_id' | 'firstName' | 'lastName' | 'dateOfBirth'> {}
+interface UpdateUserSchema extends UpdateUserInfoRequestProps {}
 interface SendSmsOtpSchema extends SendSmsOtpRequestProps {}
 interface VerifySmsOtpSchema extends VerifySmsOtpRequestProps {}
 interface PhoneNumberSchema extends Pick<UserProps, 'phoneNumber'> {}
@@ -36,15 +36,16 @@ const phoneNumberSchema = Joi.object<PhoneNumberSchema>({
 const userSchema = Joi.object<UserSchema>({
   firstName: Joi.string().default(null).trim(),
   lastName: Joi.string().default(null).trim(),
-  // password: Joi.string().pattern(new RegExp(PasswordRegex)).required(),
+  password: Joi.string().pattern(new RegExp(PasswordRegex)).allow(null, ''),
   dateOfBirth: Joi.date().iso(),
-  // address: Joi.array().items(addressSchema),
+  address: Joi.array().items(addressSchema).allow(null, ''),
   isActive: Joi.boolean().default(true),
   isVerified: Joi.boolean().default(false),
   roleID: [Joi.string().regex(ObjectIDRegex, 'valid id')],
   followerID: [Joi.string().regex(ObjectIDRegex, 'valid id')],
   followingID: [Joi.string().regex(ObjectIDRegex, 'valid id')],
   blockedID: [Joi.string().regex(ObjectIDRegex, 'valid id')],
+  avatar: Joi.string().default(null).trim().allow(null, ''),
 });
 
 const sendSmsOtpSchema = phoneNumberSchema.append<SendSmsOtpSchema>({});
@@ -56,9 +57,10 @@ const verifySmsOtpSchema = sendSmsOtpSchema.append<VerifySmsOtpSchema>({
 
 const updateUserSchema = Joi.object<UpdateUserSchema>({
   _id: idSchema.required(),
-  firstName: Joi.string().trim(),
-  lastName: Joi.string().trim(),
-  dateOfBirth: Joi.date().iso(),
+  firstName: Joi.string().trim().allow(null, ''),
+  lastName: Joi.string().trim().allow(null, ''),
+  dateOfBirth: Joi.date().iso().allow(null, ''),
+  avatar: Joi.string().trim().allow(null, ''),
 });
 
 const userModelValidation = catchErrors(async (req: Request, res: Response, next: NextFunction) => {
@@ -66,10 +68,12 @@ const userModelValidation = catchErrors(async (req: Request, res: Response, next
   next();
 });
 
-const updateUserValidation = catchErrors(async (req: Request, res: Response, next: NextFunction) => {
-  await updateUserSchema.validateAsync(req.body, { abortEarly: false });
-  next();
-});
+const updateUserValidation = catchErrors(
+  async (req: Request, res: Response, next: NextFunction) => {
+    await updateUserSchema.validateAsync(req.body, { abortEarly: false });
+    next();
+  },
+);
 
 const sendSmsOtpValidation = catchErrors(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -85,4 +89,9 @@ const verifySmsOtpValidation = catchErrors(
   },
 );
 
-export const userValidation = { sendSmsOtpValidation, userModelValidation, verifySmsOtpValidation,updateUserValidation };
+export const userValidation = {
+  sendSmsOtpValidation,
+  userModelValidation,
+  verifySmsOtpValidation,
+  updateUserValidation,
+};
