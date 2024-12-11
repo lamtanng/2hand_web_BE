699699720a43e9062b33ef536df67586d35cd6cd
lamtanng <lamnt.flamedia@gmail.com>
2024-12-11 10:48:02 +0700

    chore: formatted phone locale

--- src/services/authService/signup.service.ts
+++ src/services/authService/signup.service.ts
@@ -2,12 +2,14 @@ import { Request } from 'express';
 import { RoleModel } from '../../models/role';
 import { UserModel } from '../../models/user';
 import { Role } from '../../types/enum/role.enum';
-import { VerifyOtpRequestProps } from '../../types/http/otp.type';
+import { SignUpRequestProps } from '../../types/http/signup.type';
+import { formatPhoneNumber } from '../../utils/phone';
 
 export const signupService = async (req: Request) => {
-  const { phoneNumber, password } = req.body as VerifyOtpRequestProps;
+  const { phoneNumber, password } = req.body as SignUpRequestProps;
+  const formattedPhoneNumber = formatPhoneNumber(phoneNumber);
   const roleId = await RoleModel.findOne({ name: Role.Customer }, { _id: 1 });
-  const user = new UserModel({ phoneNumber, password, roleID: [roleId?._id] });
+  const user = new UserModel({ formattedPhoneNumber, password, roleID: [roleId?._id] });
   const newUser = await UserModel.create(user);
   return newUser;
 };
