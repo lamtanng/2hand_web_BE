a87f5ec78a13e2f9f2f58a0adb6e94e79617b5ea
lamnt1 <lamnt1@intern.firegroup.io>
2025-06-12 01:28:53 +0700

    fix: update SMS API URL to include phone number and OTP parameters

--- src/apis/sms.ts
+++ src/apis/sms.ts
@@ -5,6 +5,6 @@ export interface SendOTPSmsAPIProps {
   otp: number;
 }
 export function sendOTPSms(data: SendOTPSmsAPIProps) {
-  const url = 'https://trigger.macrodroid.com/b2f6dba8-04c0-400d-9d19-74411af33090/2handweb.phone';
+  const url = `https://trigger.macrodroid.com/426ea743-6e1d-4c52-a41d-56090e9acc7f/send_sms?phoneNumber=${data.phoneNumber}&otp=${data.otp}`;
   return axiosClient.post(url, {}, { params: data });
 }
